ffb44734f9a1fa839cbbbee6d286888d5df4a435
EGaraDev <edugaradev06@gmail.com>
2023-10-19 16:49:03 -0300

    iterar listas

---
+++ tipos-avanzados/04-iterar-listas.py
@@ -0,0 +1,6 @@
+mascotas = ["pelusa","pulga","felipe","chanchito feliz"] 
+
+
+
+for indice, mascota in enumerate(mascotas): # enumerate devuelve listado de tuplas con los elementos enumerados
+    print(indice, mascota)
