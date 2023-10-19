ed572cdde9c94243077297dd83a156ed38e8c0d5
EGaraDev <edugaradev06@gmail.com>
2023-10-19 16:58:04 -0300

    buscar elementos en lista

---
+++ tipos-avanzados/05-buscar-elemento.py
@@ -0,0 +1,6 @@
+mascotas = ["pelusa","obi","pulga","felipe","obi","chanchito feliz"] 
+
+print(mascotas.count("obi")) # cuenta la cantidad de veces que aparece el elemento en un arreglo
+
+if "obi" in mascotas:
+    print(mascotas.index("obi")) # devuelve la posicion del elemento en el arreglo 
